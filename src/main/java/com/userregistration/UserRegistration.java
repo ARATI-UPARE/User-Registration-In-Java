bca70c987e8d97056e810df53e246c304794242e
ARATI-UPARE <aratiupare@gmail.com>
2020-04-04 00:07:09 +0530

    ADDED:User Need to Enter valid EmailId

--- src/main/java/com/userregistration/UserRegistration.java
+++ src/main/java/com/userregistration/UserRegistration.java
@@ -5,8 +5,15 @@ import java.util.regex.Pattern;
 public class UserRegistration {
 
     String regPattern = "^[A-Z][a-z]{2,}$";
+    String emailPattern = "^([a-z]{2,}[0-9a-z]{1,}([_+-.*$#]{0,1}[a-z0-9]{1,}){0,1}[@]{1}[a-z0-1]{1,}[.]{1}[a-z]{2,4}([.]{0,1}[a-z]{2}){0,1})$";
+
     public  boolean validation(String name) {
 
             return Pattern.matches(regPattern,name);
+    }
+
+    public boolean emailValidation(String email) {
+
+        return Pattern.matches(emailPattern, email);
     }
 }
--- src/test/java/com/userregistration/UserRegistrationTest.java
+++ src/test/java/com/userregistration/UserRegistrationTest.java
@@ -6,7 +6,39 @@ import org.junit.Test;
 public class UserRegistrationTest {
 
     UserRegistration userRegObject = new UserRegistration(); // Object
-    // UC2
+    // UC:1
+    @Test
+    public void givenValidFirstName_shouldReturnTrue() {
+        boolean firstName=userRegObject.validation("Arati");
+        Assert.assertTrue(firstName);
+    }
+    @Test
+    public void givenFirstName_whenMinimumThreeCharacter_shouldReturnTrue() {
+        boolean firstName = userRegObject.validation("Ari");
+        Assert.assertTrue(firstName);
+    }
+    @Test
+    public void givenInvalidFirstName_shouldReturnFalse() {
+        boolean firstName=userRegObject.validation("Ar");
+        Assert.assertFalse(firstName);
+    }
+    @Test
+    public void givenFirstName_whenAllLowerCaseCharacter_shouldReturnFalse(){
+        boolean firstName = userRegObject.validation("arati");
+        Assert.assertFalse(firstName);
+    }
+    @Test
+    public void givenFirstName_whenAllUpperCaseCharacter_shouldReturnFalse() {
+        boolean firstName = userRegObject.validation("ARATI");
+        Assert.assertFalse(firstName);
+    }
+    @Test
+    public void givenFirstName_whenContainNumber_shouldReturnFalse() {
+        boolean firstName = userRegObject.validation("Ara452");
+        Assert.assertFalse(firstName);
+    }
+
+    //  UC:2
     @Test
     public void givenValidLastName_shouldReturnTrue() {
         boolean lastName=userRegObject.validation("Upare");
@@ -17,7 +49,6 @@ public class UserRegistrationTest {
         boolean lastName = userRegObject.validation("Upa");
         Assert.assertTrue(lastName);
     }
-
     @Test
     public void givenInvalidLastName_shouldReturnFalse() {
         boolean lastName=userRegObject.validation("U");
@@ -28,16 +59,45 @@ public class UserRegistrationTest {
         boolean lastName = userRegObject.validation("upare");
         Assert.assertFalse(lastName);
     }
-
     @Test
     public void givenLastName_whenAllUpperCaseCharacter_shouldReturnFalse() {
         boolean lastName = userRegObject.validation("UPARE");
         Assert.assertFalse(lastName);
     }
-
     @Test
     public void givenLastName_whenContainNumber_shouldReturnFalse() {
         boolean lastName = userRegObject.validation("Upa452");
         Assert.assertFalse(lastName);
+    }
+
+    //  UC:3
+    @Test
+    public void givenEmailId_whenProper_shouldReturnTrue() {
+        boolean emailId = userRegObject.emailValidation("aratiupare31@gmail.com");
+        Assert.assertTrue(emailId);
+    }
+
+    @Test
+    public void givenEmailId_whenContainSymbol_shouldReturnTrue() {
+        boolean emailId = userRegObject.emailValidation("arati#upare@gmail.com");
+        Assert.assertTrue(emailId);
+    }
+
+    @Test
+    public void givenEmailId_whenContainLessThanThreeCharacter_shouldReturnFalse() {
+        boolean emailId = userRegObject.emailValidation("ar@gmail.com");
+        Assert.assertFalse(emailId);
+    }
+
+    @Test
+    public void givenEmailId_whenNotContainGmailPart_shouldReturnFalse() {
+        boolean emailId = userRegObject.emailValidation("arat@.com");
+        Assert.assertFalse(emailId);
+    }
+
+    @Test
+    public void givenEmailId_whenNotContainComPart_shouldReturnFalse() {
+        boolean emailId = userRegObject.emailValidation("arati@gmail");
+        Assert.assertFalse(emailId);
     }
 }
